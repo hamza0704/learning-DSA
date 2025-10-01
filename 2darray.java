1a1799f2b994a38b8fdaedfd1c908284f73c6fb9
Hamza Shabbir <hshabbir2005@gmail.com>
2025-10-01 18:37:24 +0530

    Add files via upload

---
+++ 2darray.java
@@ -0,0 +1,11 @@
+public class 2darray {
+    public static void main(String[] args) {
+       /*  1 2 3 
+           4 5 6
+           7 8 9 
+        */
+        int [][] arr = new int[3][5];
+        
+
+    }
+}
